9ed91db206e47b563316e76c24e096c252f068cb
Xavier Twitty <x.twitty@gmail.com>
2021-08-30 19:04:55 -0400

    text not rendering to the screen

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import {Route, Link , Switch } from "react-router-dom"
+
+const App = () => {
+return (
+<div classname="App">
+    <h1>hello world</h1>
+                                                                                                                                                                                                                                                                                  
+</div>
+)
+}
+
+
+export default App   
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { render } from "react-dom"
-
-import App from "./components/App"
+import App from "./App"
 
 import { BrowserRouter as Router } from "react-router-dom"
 
