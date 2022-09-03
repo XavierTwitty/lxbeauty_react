2599fbb29a6c0f0a7a538198870db8e7b5db3839
XavierTwitty <x.twitty@gmail.com>
2022-09-03 09:15:50 -0700

    added training data

---
+++ trainingData.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    title: "One Day Classic Lash",
+    p1: "Private one day class for classic lash extension training.",
+    p2: "Includes safety & sanitation, product resources, isolation, pick-up, placement, tweezer practice, business and marketing, and full set practice on a mannequin. ",
+    p3: "Comes with a fully loaded kit.",
+    hours: "5 hr",
+    price: "$850",
+  },
+  {
+    id: 2,
+    title: "One Day Classic Lash",
+    p1: "",
+    p2: "",
+    p3: "",
+    hours: "2 Days",
+    price: "$1000",
+  },
+];
