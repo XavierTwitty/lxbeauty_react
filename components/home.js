47c8c55f05bdaf44d8f2ec76d3e7783240e5373e
XavierTwitty <x.twitty@gmail.com>
2022-09-29 06:36:42 -0700

    deleted extra images

--- components/home.js
+++ components/home.js
@@ -10,24 +10,6 @@ function home() {
             alt="eyelash picture"
           />
         </div>
-        <div class="mySlides fade">
-          <img
-            src="https://images.unsplash.com/photo-1522390108011-5f8667fd551d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-            alt="eyelash picture"
-          />
-        </div>
-        <div class="mySlides fade">
-          <img
-            src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-            alt="eyelash picture"
-          />
-        </div>
-        <div class="mySlides fade">
-          <img
-            src="https://images.unsplash.com/photo-1512413316925-fd4b93f31521?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-            alt="eyelash picture"
-          />
-        </div>
       </div>
       <h3>Making beauty personal</h3>
     </section>
