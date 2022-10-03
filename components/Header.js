1484949b4550499563884d19dbd49ba79452dcf8
XavierTwitty <x.twitty@gmail.com>
2022-10-03 16:28:19 -0700

    Header css

--- components/Header.js
+++ components/Header.js
@@ -3,38 +3,10 @@ import Navigation from "./Navigation";
 
 function Header() {
   return (
-    <div
-      className="header-container"
-      style={{
-        display: "flex",
-        width: "100%",
-        height: "20rem",
-        marginTop: "3%",
-      }}
-    >
-      <div
-        className="titles"
-        style={{
-          width: "50%",
-          textAlign: "center",
-        }}
-      >
-        <h1
-          className="h1"
-          style={{
-            textAlign: "center",
-            fontFamily: "fantasy",
-            fontSize: "5rem",
-          }}
-        >
-          {" "}
-          Loxurious Beauty{" "}
-        </h1>
-        <h2
-          style={{
-            fontFamily: "fantasy",
-          }}
-        >
+    <div className="header-container">
+      <div className="titles">
+        <h1> Loxurious Beauty </h1>
+        <h2>
           Lashes, brows &<br />
           permanent make-up
         </h2>
